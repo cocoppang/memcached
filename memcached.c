922d67a529c01ec69032232cc0610449b3bab0a6
Taehoon Kim <cearial09@gmail.com>
2019-01-12 15:52:13 +0900

    Reduce the usage of memory for reading file to instr buffer

--- memcached.c
+++ memcached.c
@@ -130,6 +130,8 @@ conn **conns;
 
 time_t start1, end1, mid1;
 int set_size = 10000000;
+int set_size2 = 10000000;
+int set_jitter_size = 100000;
 //int set_size = 500;
 
 struct slab_rebalance slab_rebal;
@@ -5288,8 +5290,8 @@ static enum try_read_result try_read_file(conn *c) {
 
 	for(i = 0; i < settings.num_threads;i++) {
 		c->inst_count[i] = 0;
-		c->inst[i] = (char**)malloc(sizeof(char*)*set_size);
-		for(j = 0; j < set_size;j++) {
+		c->inst[i] = (char**)malloc(sizeof(char*)*(set_size/settings.num_threads + set_jitter_size));
+		for(j = 0; j < set_size/settings.num_threads + set_jitter_size;j++) {
 			c->inst[i][j] = (char*)malloc(sizeof(char)*settings.buffer_size);
 		}
 	}
@@ -5299,8 +5301,8 @@ static enum try_read_result try_read_file(conn *c) {
 
 	for(i = 0; i < settings.num_threads;i++) {
 		c->inst_count2[i] = 0;
-		c->inst2[i] = (char**)malloc(sizeof(char*)*set_size);
-		for(j = 0; j < set_size;j++) {
+		c->inst2[i] = (char**)malloc(sizeof(char*)*(set_size2/settings.num_threads + set_jitter_size));
+		for(j = 0; j < set_size2/settings.num_threads + set_jitter_size;j++) {
 			c->inst2[i][j] = (char*)malloc(sizeof(char)*settings.buffer_size);
 		}
 	}
@@ -5338,7 +5340,7 @@ static enum try_read_result try_read_file(conn *c) {
 	f2 = fopen(filename,"r");
 
 	i = 0;
-	while(i < set_size) {
+	while(i < set_size2) {
 		if(fgets(buf, settings.buffer_size, f2) == NULL) {
 			fprintf(stderr, "run file read error\n");
 			return READ_NO_DATA_RECEIVED;
